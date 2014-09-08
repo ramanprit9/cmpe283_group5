937675c76eeb6f4bcdb4de462a15655afdb44a93
susmithamenda <susmithamenda@gmail.com>
2014-09-07 21:24:27 -0700

    Create Job.java

---
+++ app/models/Job.java
@@ -0,0 +1,27 @@
+package models;
+
+public class Job {
+	private String jobid;
+	private String jobclassname;
+	private String json;
+	
+	public String getJobid() {
+		return jobid;
+	}
+	public void setJobid(String jobid) {
+		this.jobid = jobid;
+	}
+	public String getJobclassname() {
+		return jobclassname;
+	}
+	public void setJobclassname(String jobclassname) {
+		this.jobclassname = jobclassname;
+	}
+	public String getJson() {
+		return json;
+	}
+	public void setJson(String json) {
+		this.json = json;
+	}
+
+}
