83d16044b5995cd345143df3c3a777f829c12d2e
susmithamenda <susmithamenda@gmail.com>
2014-09-07 21:25:16 -0700

    Create ResourceUsage.java

---
+++ app/models/ResourceUsage.java
@@ -0,0 +1,43 @@
+package models;
+
+public class ResourceUsage {
+	
+	private String userid;
+	private String serviceid;
+	private String resourceid;
+	private String starttime;
+	private String endtime;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getServiceid() {
+		return serviceid;
+	}
+	public void setServiceid(String serviceid) {
+		this.serviceid = serviceid;
+	}
+	public String getResourceid() {
+		return resourceid;
+	}
+	public void setResourceid(String resourceid) {
+		this.resourceid = resourceid;
+	}
+	public String getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+	public String getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(String endtime) {
+		this.endtime = endtime;
+	}
+	
+	
+
+}
