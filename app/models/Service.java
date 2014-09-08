3ea6d45c4c75be00d1057ee191ea61d2b301e7a6
susmithamenda <susmithamenda@gmail.com>
2014-09-07 21:25:38 -0700

    Create Service.java

---
+++ app/models/Service.java
@@ -0,0 +1,57 @@
+package models;
+
+public class Service {
+	
+	private String serviceid;
+	private String servicename;
+	private String uid;
+	private String servicetype;
+	private String status;
+	private String datecreated;
+	private String json;
+	
+	public String getServiceid() {
+		return serviceid;
+	}
+	public void setServiceid(String serviceid) {
+		this.serviceid = serviceid;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getServicetype() {
+		return servicetype;
+	}
+	public void setServicetype(String servicetype) {
+		this.servicetype = servicetype;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getDatecreated() {
+		return datecreated;
+	}
+	public void setDatecreated(String datecreated) {
+		this.datecreated = datecreated;
+	}
+	public String getJson() {
+		return json;
+	}
+	public void setJson(String json) {
+		this.json = json;
+	}
+	public String getServicename() {
+		return servicename;
+	}
+	public void setServicename(String servicename) {
+		this.servicename = servicename;
+	}
+	
+
+}
