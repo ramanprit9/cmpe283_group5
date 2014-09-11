de279a763defaa0e960f8488ee7a765281e328c7
susmithamenda <susmithamenda@gmail.com>
2014-09-11 00:39:42 -0700

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,23 +1,16 @@
 package controllers;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
-
-import javax.sql.DataSource;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 import models.DataAccess;
 import models.Service;
 import models.User;
-import models.Website;
 import models.WebsiteHandler;
 import play.Logger;
-import play.api.mvc.Session;
-import play.db.*;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.*;
@@ -25,116 +18,142 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-    	
-    	
-    	ArrayList<String> webservices = new ArrayList<String>();
-    	ArrayList<String> dbservices = new ArrayList<String>();
-    	
-    	webservices.add("jason");
-    	webservices.add("website");
-    	
-    	dbservices.add("mysql");
-    	dbservices.add("postgres");
-    	
-        return ok(index.render("Hello World!"));
-    }
-    
-    public static Result hello() {
-        return ok("Hello World");
-    }
-    
-    public static Result dashboard(){
-    	
-    	DataAccess db = new DataAccess();
-    	HashMap<String,ArrayList<String>> allservices = new HashMap<String,ArrayList<String>>();
-    	ArrayList<String> webservices = new ArrayList<String>();
-    	ArrayList<String> dbservices = new ArrayList<String>();
-    	try {
-			allservices = db.getAll();
+	public static Result index() {
+
+
+		ArrayList<String> webservices = new ArrayList<String>();
+		ArrayList<String> dbservices = new ArrayList<String>();
+
+		webservices.add("jason");
+		webservices.add("website");
+
+		dbservices.add("mysql");
+		dbservices.add("postgres");
+
+		return ok(dashboard.render(webservices,dbservices));
+	}
+
+	public static Result dashboard(){
+
+		Logger.debug("------Entered into Dashboard() method in Application Controller------");
+		DataAccess db = new DataAccess();
+		HashMap<String,ArrayList<String>> allservices = new HashMap<String,ArrayList<String>>();
+		ArrayList<String> webservices = new ArrayList<String>();
+		ArrayList<String> dbservices = new ArrayList<String>();
+		try {
+			allservices = db.getAllServices();
 			if(allservices != null){
 				webservices = allservices.get("webservice");
 				dbservices = allservices.get("dbservice");
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (Exception e) {
+			Logger.error("ERROR While fetching services from database: "+ e.getMessage());
 		}
-    	return ok(dashboard.render(webservices,dbservices));
-    }
-    public static Result createWebsite() throws SQLException {
-    	
-    	String name = null;
-    	String plan = null;
-    	WebsiteHandler job = null;
-    	int status = 0;
-    	DataAccess db = new DataAccess();
-    	Service service = new Service();
-    	Date date = new Date();
-    	JsonNode json = request().body().asJson();
-    	Logger.debug("json request is:"+ json);
-    	
-    	
-    	//retrieving user session
-    	String user = session("username");
-    	String uid = session("uid");
-    	Logger.debug("User ID: "+ user);
-   	
-    	if(json != null) {
-    	    name = json.findPath("name").textValue();
-    	    service.setServicename(name);
-    	    plan = json.findPath("plan").textValue();
-    	    //determining service type
-    	    if(plan.equals("gold")){
-    	    	service.setServicetype("smallWebsite");
-    	    }else{
-    	    	service.setServicetype("bigWebsite");
-    	    }
-    	    status = db.saveWebsitetoServices(uid, service.getServicename(), service.getServicetype(),date.toString(), json.toString());
-    	    service = db.getServiceByName(service.getServicename());
-    	    String jobjson = Json.toJson(service).toString();
-    	    job = new WebsiteHandler(service);
-    	    status = job.save(jobjson);
-    	  }
-    	return ok(createWebsite.render("Hello World"));
-    }
-    public static Result signup(){
-    	return ok(signup.render("Hello World"));
-    }
-    public static Result createuser(){
-    	
-    	Form<User> UserForm = Form.form(User.class);
-    	User user = UserForm.bindFromRequest().get();
-        DataAccess da = new DataAccess();
-        try {
-        	int status = 0;
+		Logger.debug("------Exit from Dashboard() method in Application Controller------");
+		return ok(dashboard.render(webservices,dbservices));
+	}
+	public static Result createWebsite()  {
+
+		Logger.debug("------Entered into createWebsite() method in Application Controller------");
+		String name = null;
+		String plan = null;
+		WebsiteHandler job = null;
+		DataAccess db = new DataAccess();
+		Service service = new Service();
+		Date date = new Date();
+		JsonNode json = request().body().asJson();
+		Logger.debug("json request is:"+ json);
+
+		try{
+			//retrieving user session
+			String user = session("username");
+			String uid = session("uid");
+			Logger.debug("User ID: "+ user);
+
+			if(json != null) {
+				name = json.findPath("name").textValue();
+				service.setServicename(name);
+				plan = json.findPath("plan").textValue();
+				//determining service type
+				if(plan.equals("gold")){
+					service.setServicetype("smallWebsite");
+				}else{
+					service.setServicetype("bigWebsite");
+				}
+				int status = db.saveToServices(uid, service.getServicename(), service.getServicetype(),date.toString(), json.toString());
+				if(status == 0){
+					throw new Exception("Could not create service");
+				}
+				service = db.getServiceByName(service.getServicename());
+				if(service == null){
+					throw new Exception("service doesnot exist");
+				}
+				String jobjson = Json.toJson(service).toString();
+				job = new WebsiteHandler(service);
+				int jobstatus = job.save(jobjson);
+				if(jobstatus ==0){
+					throw new Exception("Could not create job");
+				}
+			}
+		}catch (Exception e) {
+			Logger.error("ERROR while fetching or saving service details from database: "+ e.getMessage());
+		}
+
+		Logger.debug("------Exit from createWebsite() method in Application Controller------");
+		return ok(createWebsite.render("Hello World"));
+	}
+	public static Result signup(){
+		Logger.debug("------Entered into signup() method in Application Controller------");
+
+		Logger.debug("------Exit from signup() method in Application Controller------");
+		return ok(signup.render("SignUp"));
+	}
+	public static Result createuser(){
+
+		Logger.debug("------Entered into createuser() method in Application Controller------");
+		Form<User> UserForm = Form.form(User.class);
+		User user = UserForm.bindFromRequest().get();
+		DataAccess da = new DataAccess();
+		try {
+			int status = 0;
 			status = da.createUser(user);
-		} catch (SQLException e) {
-			e.printStackTrace();
+			if(status == 0){
+				throw new Exception("Could not create user");
+			}
+		} catch (Exception e) {
+			Logger.error("ERROR while creating user: "+ e.getMessage());
 		}
-    	return ok(index.render("User Created"));
-    }
-    public static Result login(){
-    	return ok(login.render("Hello World"));
-    }
-    public static Result loginUser(){
-    	
-    	Form<User> UserForm = Form.form(User.class);
-    	User user = UserForm.bindFromRequest().get();
-    	DataAccess da = new DataAccess();
-    	User user1 = new User();
-    	try {
+		Logger.debug("------Exit from createuser() method in Application Controller------");
+		return ok(index.render("User Created"));
+	}
+	public static Result login(){
+		Logger.debug("------Entered into login() method in Application Controller------");
+
+		Logger.debug("------Exit from login() method in Application Controller------");
+		return ok(login.render("Login Page"));
+	}
+	public static Result loginUser(){
+
+		Logger.debug("------Entered into loginUser() method in Application Controller------");
+		Form<User> UserForm = Form.form(User.class);
+		User user = UserForm.bindFromRequest().get();
+		DataAccess da = new DataAccess();
+		User user1 = new User();
+		try {
 			user1 = da.getUser(user);
 			if(user1!= null)
 			{
 				session("username", user.getUsername());
 				session("uid", user.getUid());
 				return redirect("/dashboard");
+			}else{
+				throw new Exception("user doesnot exist");
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			Logger.error("ERROR while fetching user details from database: "+ e.getMessage());
 		}
-    	return ok(login.render("Hello World"));
-    }
-    
+		Logger.debug("------Exit from loginUser() method in Application Controller------");
+		return ok(login.render("Welcome"));
+	}
+
 }
