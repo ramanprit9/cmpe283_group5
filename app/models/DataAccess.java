b1ae5603dca3ee49184bc7b612724cddb41d2f01
susmithamenda <susmithamenda@gmail.com>
2014-09-11 00:40:51 -0700

    Update DataAccess.java

--- app/models/DataAccess.java
+++ app/models/DataAccess.java
@@ -5,188 +5,221 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 import play.Logger;
 import play.db.DB;
 
 public class DataAccess {
 	Statement stmt = null;
-    Connection connection = null;
-    
-    public int saveWebsitetoServices(String uid, String servicename, String servicetype,String date, String json) throws SQLException{
-    	int status = 0;
-    	connection = DB.getConnection();
-        try {
+	Connection connection = null;
+
+	public int saveToServices(String uid, String servicename, String servicetype,String date, String json) throws SQLException{
+
+		Logger.debug("------Entered into saveToServices("+uid+","+servicename+","+servicetype+","+date+","+json+") method in DataAccess------");
+		int status = 0;
+		connection = DB.getConnection();
+		try {
 			stmt = connection.createStatement();
 			String sql;
-	        sql = "INSERT INTO services VALUES('"+ uid +"','"+ servicename +"','"+servicetype +","+ "'created'"+","+ date.toString() + json +")";
-	        Logger.debug("Inserting into services table: " + sql);
-	        status = stmt.executeUpdate(sql);
-	        connection.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        finally{
-        	connection.close();
-        }
-        return status;
-    }
-    public Service getServiceByName(String name) throws SQLException{
-    	int status = 0;
-    	connection = DB.getConnection();
-    	Service service = new Service();
-        try {
-			stmt = connection.createStatement();
-			String sql;
-	        sql = "SELECT * FROM services WHERE servicename="+name;
-	        ResultSet rs = stmt.executeQuery(sql);
-    		while(rs.next()){
-    	         //Retrieve by column name
-    	         String serviceid  = rs.getString("serviceid");
-    	         Integer uid = rs.getInt("uid");
-    	         service.setUid(uid.toString());
-    	         service.setJson(rs.getString("json"));
-    	         service.setDatecreated(rs.getString("datecreated"));
-    	         service.setServiceid(serviceid);
-    	         service.setServicetype(rs.getString("servicetype"));
-    	         service.setStatus(rs.getString(status));
-    	         service.setServicename(rs.getString("servicename"));
-    	      }
-    	      rs.close();
-	        connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-        finally{
-        	connection.close();
-        }
-        return service;
-    }
-    public int createUser(User user) throws SQLException{
-    	int status =0;
-    	connection = DB.getConnection();
-    	try{
-    		stmt = connection.createStatement();
-    		String sql;
-    		sql = "INSERT INTO users VALUES('"+user.getUsername()+"','"+user.getPassword()+")";
-    		Logger.debug("inserting into users table: "+sql);
-    		status = stmt.executeUpdate(sql);
-    		connection.close();
-    	}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        finally{
-        	connection.close();
-        }
-        return status;
-    }
-    public User getUser(User user) throws SQLException{
-    	connection = DB.getConnection();
-    	try{
-    		stmt = connection.createStatement();
-    		String sql;
-    		sql = "SELECT * FROM USERS WHERE username=" + user.getUsername() +"AND password="+ user.getPassword()+")";
-    		Logger.debug("inserting into users table: "+sql);
-    		ResultSet rs = stmt.executeQuery(sql);
-    		while(rs.next()){
-    	         //Retrieve by column name
-    	         Integer id  = rs.getInt("uid");
-    	         user.setUid(id.toString());
-    	         user.setUsername(rs.getString("username"));
-    	      }
-    	      rs.close();
-    		connection.close();
-    	}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        finally{
-        	connection.close();
-        }
-        return user;
-    }
-    public int saveWebsiteJob(String json) throws SQLException{
-    	int status =0;
-    	connection = DB.getConnection();
-    	try{
-    		stmt = connection.createStatement();
-    		String sql;
-    		sql= "INSERT INTO JOBS  VALUES('WebsiteHandler.class',"+json + ")";
+			sql = "INSERT INTO services VALUES('"+ uid +"','"+ servicename +"','"+servicetype +","+ "'created'"+","+ date.toString() + json +")";
+			Logger.debug("Inserting into services table: " + sql);
 			status = stmt.executeUpdate(sql);
-    		Logger.debug("inserting into jobs table: "+sql);
-    		status = stmt.executeUpdate(sql);
-    		connection.close();
-    	}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			connection.close();
+		} catch (Exception e) {
+			Logger.error("ERROR while saving website to service table: "+ e.getMessage());
 		}
-        finally{
-        	connection.close();
-        }
-        return status;
-    }
-    public int deleteWebsiteJob(String jobid) throws SQLException{
-    	int status =0;
-    	connection = DB.getConnection();
-    	try{
-    		stmt = connection.createStatement();
-    		String deleteSQL = "DELETE FROM JOBS WHERE jobid ="+jobid;
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Exit from saveToServices() method in DataAccess------");
+		return status;
+	}
+	public Service getServiceByName(String name) throws SQLException{
+		Logger.debug("------Entered into getServiceByName("+name+") method in DataAccess------");
+		int status = 0;
+		connection = DB.getConnection();
+		Service service = new Service();
+		try {
+			stmt = connection.createStatement();
+			String sql;
+			sql = "SELECT * FROM services WHERE servicename="+name;
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				//Retrieve by column name
+				String serviceid  = rs.getString("serviceid");
+				Integer uid = rs.getInt("uid");
+				service.setUid(uid.toString());
+				service.setJson(rs.getString("json"));
+				service.setDatecreated(rs.getString("datecreated"));
+				service.setServiceid(serviceid);
+				service.setServicetype(rs.getString("servicetype"));
+				service.setStatus(rs.getString(status));
+				service.setServicename(rs.getString("servicename"));
+			}
+			rs.close();
+			connection.close();
+		} catch (Exception e) {
+			Logger.error("ERROR while fetching service details from services table: "+ e.getMessage());
+		}
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Entered into getServiceByName() method in DataAccess------");
+		return service;
+	}
+	public int createUser(User user) throws SQLException {
+
+		Logger.debug("------Entered into createUser() for "+user.getUsername());
+		int status =0;
+		connection = DB.getConnection();
+		try{
+			stmt = connection.createStatement();
+			String sql;
+			sql = "INSERT INTO users VALUES('"+user.getUsername()+"','"+user.getPassword()+")";
+			Logger.debug("inserting into users table: "+sql);
+			status = stmt.executeUpdate(sql);
+			connection.close();
+		}catch (Exception e) {
+			Logger.error("ERROR while creating new user to users table: "+ e.getMessage());
+		}
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Exit from createUser() method in DataAccess");
+		return status;
+	}
+	public User getUser(User user) throws SQLException{
+
+		Logger.debug("------Entered into getUser() for "+user.getUsername());
+		connection = DB.getConnection();
+		try{
+			stmt = connection.createStatement();
+			String sql;
+			sql = "SELECT * FROM USERS WHERE username=" + user.getUsername() +"AND password="+ user.getPassword()+")";
+			Logger.debug("inserting into users table: "+sql);
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				//Retrieve by column name
+				Integer id  = rs.getInt("uid");
+				user.setUid(id.toString());
+				user.setUsername(rs.getString("username"));
+			}
+			rs.close();
+			connection.close();
+		}catch (Exception e) {
+			Logger.error("ERROR while fetching user details from users table: "+ e.getMessage());
+		}
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Exit from getUser() method in DataAccess");
+		return user;
+	}
+	public int saveToJob(String json) throws SQLException{
+
+		Logger.debug("------Entered into saveToJob()--json: +"+json);
+		int status =0;
+		connection = DB.getConnection();
+		try{
+			stmt = connection.createStatement();
+			String sql;
+			sql= "INSERT INTO JOBS  VALUES('WebsiteHandler.class',"+json + ")";
+			status = stmt.executeUpdate(sql);
+			Logger.debug("inserting into jobs table: "+sql);
+			status = stmt.executeUpdate(sql);
+			connection.close();
+		}catch (Exception e) {
+			Logger.error("ERROR while saving service to jobs table: "+ e.getMessage());
+		}
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Exit from saveToJob() method in DataAccess");
+		return status;
+	}
+	public int deleteJob(String jobid) throws SQLException{
+
+		Logger.debug("------Entered into deleteJob()--jobid: +"+jobid);
+		int status =0;
+		connection = DB.getConnection();
+		try{
+			stmt = connection.createStatement();
+			String deleteSQL = "DELETE FROM JOBS WHERE jobid ="+jobid;
 			status = stmt.executeUpdate(deleteSQL);
-    		Logger.debug("inserting into jobs table: "+deleteSQL);
-    		status = stmt.executeUpdate(deleteSQL);
-    		connection.close();
-    	}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Logger.debug("inserting into jobs table: "+deleteSQL);
+			status = stmt.executeUpdate(deleteSQL);
+			connection.close();
+		}catch (Exception e) {
+			Logger.error("ERROR while deleting service from jobs table: "+ e.getMessage());
 		}
-        finally{
-        	connection.close();
-        }
-        return status;
-    }
-    
-    public HashMap<String,ArrayList<String>> getAll() throws SQLException{
-    	
-    	ArrayList<String> webservice = new ArrayList<String>();
-    	ArrayList<String> dbservice = new ArrayList<String>();
-    	HashMap<String,ArrayList<String>> allservices = new HashMap<String,ArrayList<String>>();
-    	
-    	connection = DB.getConnection();
-    	try{
-    		stmt = connection.createStatement();
-    		String sql;
-    		sql = "SELECT * FROM SERVICES";
-    		ResultSet rs = stmt.executeQuery(sql);
-    		while(rs.next()){
-    	         //Retrieve by column name
-    			
-    	         String servicetype  = rs.getString("servicetype");
-    	         String servicename = rs.getString("servicename");
-    	         
-    	         if(servicetype.contains("website")){
-    	        	 webservice.add(servicename);
-    	         }
-    	         if(servicetype.contains("database")){
-    	        	 dbservice.add(servicename);
-    	         }
-    	      }
-    		allservices.put("webservice", webservice);
-    		allservices.put("dbservice",dbservice);
-    	      rs.close();
-    		connection.close();
-    	}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		finally{
+			connection.close();
 		}
-        finally{
-        	connection.close();
-        }
-        return allservices;
-    }
+		Logger.debug("------Exit from deleteJob() method in DataAccess");
+		return status;
+	}
+
+	public HashMap<String,ArrayList<String>> getAllServices() throws SQLException{
+
+		Logger.debug("------Entered into getAllServices() method in DataAccess -------fetch all existing services for user");
+		ArrayList<String> webservice = new ArrayList<String>();
+		ArrayList<String> dbservice = new ArrayList<String>();
+		HashMap<String,ArrayList<String>> allservices = new HashMap<String,ArrayList<String>>();
+
+		connection = DB.getConnection();
+		try{
+			stmt = connection.createStatement();
+			String sql;
+			sql = "SELECT * FROM SERVICES";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				//Retrieve by column name
+
+				String servicetype  = rs.getString("servicetype");
+				String servicename = rs.getString("servicename");
+
+				if(servicetype.contains("website")){
+					webservice.add(servicename);
+				}
+				if(servicetype.contains("database")){
+					dbservice.add(servicename);
+				}
+			}
+			allservices.put("webservice", webservice);
+			allservices.put("dbservice",dbservice);
+			rs.close();
+			connection.close();
+		}catch (Exception e) {
+			Logger.error("ERROR while fetching all the services for the user from services table: "+ e.getMessage());
+		}
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Exit from getAllServices() method in DataAccess -------");
+		return allservices;
+	}
+	public int addResource(String resourcetype , Resource resource, Service service) throws SQLException{
+
+		Logger.debug("------Exit from addResource() method in DataAccess -------");
+		int status =0;
+		connection = DB.getConnection();
+		try{
+			stmt = connection.createStatement();
+			String sql;
+			sql= "INSERT INTO resources VALUES("+service.getServiceid()+"'"+resourcetype+"'"+"'"+resource.getStatus()+"'"+service.getDatecreated()+"'"+resource.getJson()+"')";
+			status = stmt.executeUpdate(sql);
+			Logger.debug("inserting into resources table: "+sql);
+			status = stmt.executeUpdate(sql);
+			connection.close();
+		}catch (Exception e) {
+			Logger.error("ERROR while saving resource to resources table: "+ e.getMessage());
+		}
+		finally{
+			connection.close();
+		}
+		Logger.debug("------Exit from addResource() method in DataAccess -------");
+		return status;
+	}
 
 }
