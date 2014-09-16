4c8cbafb24abea4612d73a852c2a098b0d0349f5
susmithamenda <susmithamenda@gmail.com>
2014-09-16 12:07:23 -0700

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,19 @@
 package controllers;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import models.BackgroundScheduler;
 import models.DataAccess;
-import models.Service;
+import models.DatabaseHandler;
+import models.Services;
 import models.User;
+import models.Website;
 import models.WebsiteHandler;
 import play.Logger;
 import play.Routes;
@@ -22,16 +27,10 @@ public class Application extends Controller {
 	public static Result index() {
 
 		Logger.debug("------Entered into index() method in Application Controller------");
-		ArrayList<String> webservices = new ArrayList<String>();
-		ArrayList<String> dbservices = new ArrayList<String>();
-
-		webservices.add("jason");
-		webservices.add("website");
-
-		dbservices.add("mysql");
-		dbservices.add("postgres");
+		//BackgroundScheduler backgroundScheduler = new BackgroundScheduler();
+		//backgroundScheduler.jobs();
 		Logger.debug("------Entered into index() method in Application Controller------");
-		return ok(dashboard.render(webservices,dbservices , session("username")));
+		return ok(" ");
 	}
 
 	public static Result dashboard(){
@@ -53,35 +52,43 @@ public class Application extends Controller {
 
 		return ok(dashboard.render(webservices,dbservices , session("username")));
 	}
-	public static Result createWebsite()  {
+	public static Result showcreateWebsite(){
+		Logger.debug("------Entered into showcreateWebsite() method in Application Controller------");
 
-		Logger.debug("------Entered into createWebsite() method in Application Controller------");
+		Logger.debug("------Exit from showcreateWebsite() method in Application Controller------");
+		return ok(createWebsite.render(" ", session("username"),session("uid")));
+	}
+	public static Result createWebsite() {
+
 		String name = null;
 		String plan = null;
-		WebsiteHandler job = null;
+		Logger.debug("------Entered into createWebsite() method in Application Controller------");
+		//WebsiteHandler job = null;
 		DataAccess db = new DataAccess();
-		Service service = new Service();
+		Services service = new Services();
 		Date date = new Date();
-		JsonNode json = request().body().asJson();
+		Form<Website> WebsiteForm = Form.form(Website.class);
+		Website website = WebsiteForm.bindFromRequest().get();
+		ObjectNode json = Json.newObject();
+		json.put("name", website.getName());
+		json.put("plan", website.getPlan());
 		Logger.debug("json request is:"+ json);
 		//retrieving user session
 		String user = session("username");
 		String uid = session("uid");
 
 		try{
-			
-			Logger.debug("User ID: "+ user);
+
+			Logger.debug("UserName: "+ user);
+			Logger.debug("name: "+ name);
+			Logger.debug("plan: "+ plan);
 
 			if(json != null) {
 				name = json.findPath("name").textValue();
 				service.setServicename(name);
 				plan = json.findPath("plan").textValue();
-				//determining service type
-				if(plan.equals("gold")){
-					service.setServicetype("smallWebsite");
-				}else{
-					service.setServicetype("bigWebsite");
-				}
+				service.setServicetype(plan);
+
 				int status = db.saveToServices(uid, service.getServicename(), service.getServicetype(),date.toString(), json.toString());
 				if(status == 0){
 					throw new Exception("Could not create service");
@@ -90,20 +97,30 @@ public class Application extends Controller {
 				if(service == null){
 					throw new Exception("service doesnot exist");
 				}
-				String jobjson = Json.toJson(service).toString();
-				job = new WebsiteHandler(service);
-				int jobstatus = job.save(jobjson);
+				Logger.debug("getservice exit in app controller");
+
+
+				ObjectNode jsonservice = Json.newObject();
+				jsonservice.put("serviceid", service.getServiceid());
+				jsonservice.put("servicename", service.getServicename());
+				jsonservice.put("uid", service.getUid());
+				jsonservice.put("servicetype", service.getServicetype());
+				jsonservice.put("status", service.getStatus());
+				jsonservice.put("datecreated", service.getDatecreated());
+
+				WebsiteHandler job = new WebsiteHandler(service);
+				int jobstatus = job.save(jsonservice.toString());
 				if(jobstatus ==0){
 					throw new Exception("Could not create job");
 				}
 			}
 		}catch (Exception e) {
-			Logger.error("ERROR while fetching or saving service details from database: "+ e.getMessage());
-			return ok(createWebsite.render(e.getMessage(), user , uid));
+			e.printStackTrace();
+			return redirect("/dashboard");
 		}
 
 		Logger.debug("------Exit from createWebsite() method in Application Controller------");
-		return ok(createWebsite.render("success", user, uid));
+		return redirect("/dashboard");
 	}
 	public static Result signup(){
 		Logger.debug("------Entered into signup() method in Application Controller------");
@@ -142,11 +159,7 @@ public class Application extends Controller {
 		Form<User> UserForm = Form.form(User.class);
 		User user = UserForm.bindFromRequest().get();
 		DataAccess da = new DataAccess();
-		
-		/*webservices.add("website");
 
-		dbservices.add("mysql");
-		dbservices.add("postgres");*/
 		User user1 = new User();
 		try {
 			user1 = da.getUser(user);
@@ -164,17 +177,77 @@ public class Application extends Controller {
 		Logger.debug("------Exit from loginUser() method in Application Controller------");
 		return redirect("/dashboard");
 	}
-	 /*public static Result bigWebService() {
-	        return ok(bigWebService.render("Hello World"));
-	    }
-	   
-	    public static Result smallWebService() {
-	        return ok(websiteDetails.render("Hello World"));
-	    }
-	   
-	    public static Result dbService() {
-	        return ok(dbService.render("Hello World"));
-	    }*/
-	
+	public static Result logout(){
+		session().clear();
+		return redirect("/login");
+	}
+	public static Result startVM(){
+		session().clear();
+		return redirect("/");
+	}
+	public static Result stopVM(){
+		session().clear();
+		return redirect("/");
+	}
+	public static Result createDatabase() {
+		Logger.debug("------Entered into createDatabase() method in Application controller------");
+		String name = null;
+		DatabaseHandler job = null;
+		int status = 0;
+		DataAccess db = new DataAccess();
+		Services service = new Services();
+		Date date = new Date();
+		JsonNode json = request().body().asJson();
+		Logger.debug("json request is:"+ json);
 
+
+		//retrieving user session
+		String user = session("username");
+		String uid = session("uid");
+		Logger.debug("User ID: "+ user);
+		try{
+			if(json != null) {
+				name = json.findPath("DatabaseName").textValue();
+				System.out.println(name);
+				service.setServicename(name);
+			}
+			service.setServicetype("DatabaseService");
+			status = db.saveToServices(uid, service.getServicename(), service.getServicetype(),date.toString(), json.toString());
+			if(status==0)
+			{
+				throw new Exception("Could not create service");
+			}
+			service = db.getServiceByName(service.getServicename());
+			if(service==null)
+			{
+				throw new Exception("Service does not exist");
+			}
+			String jobjson = Json.toJson(service).toString();
+			job = new DatabaseHandler(service);
+			int jobstatus = job.save(jobjson);
+			if(jobstatus == 0)
+			{
+				throw new Exception("Could not create service");
+			}
+		}
+		catch(SQLException sqle){
+			
+		}
+		catch (Exception e) {
+			Logger.error("ERROR while fetching or saving service details from database: "+ e.getMessage());
+			return ok(createWebsite.render(e.getMessage(), user , uid));
+		}
+		Logger.debug("------Exit from createDatabase() method in Application Controller------");
+		return redirect("routes.Application.dashboard()");
+
+	}
+
+
+	public static Result fetchcreateDatabasepage(){
+		Logger.debug("------Entered into fetchcreateDatabasepage() method in Application controller------");
+		Logger.debug("------Exit from fetchcreateDatabasepage() method in Application controller------");
+
+		return ok(createDatabase.render("Hello World"));
+
+	}  
 }
